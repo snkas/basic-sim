4f860ecef456e12459db9f031fd6342df1835bb1
snkas <isniska@gmail.com>
2020-07-08 13:54:24 +0200

    Add test stage messages

--- build/test.sh
+++ build/test.sh
@@ -6,10 +6,13 @@ mkdir -p test_results
 cd ${NS3_VERSION} || exit 1
 
 # Empty coverage counters
+echo "Zeroing coverage counters"
 lcov --directory build/debug_all --zerocounters
 
 # Perform coverage test
+echo "Performing tests for coverage"
 python test.py -v -s "basic-sim" -t ../test_results/test_results_basic_sim
 
 # Show results
+echo "Display test results"
 cat ../test_results/test_results_basic_sim.txt
