3c8ef570d7f9617abd17d77dd80b5160457dea95
snkas <isniska@gmail.com>
2021-01-14 00:18:04 +0100

    Most interesting plots first in one_tcp and one_udp

--- build/one_tcp.sh
+++ build/one_tcp.sh
@@ -13,6 +13,11 @@ bash run_assist.sh "example_run_folders/one_tcp" 0 || exit 1
 # Go plot helpers
 cd ../tools/plotting || exit 1
 
+# TCP flow plots
+cd plot_tcp_flow || exit 1
+python plot_tcp_flow.py ../../../build/example_run_folders/one_tcp/logs_ns3 ../../../build/example_run_folders/one_tcp/logs_ns3/data ../../../build/example_run_folders/one_tcp/logs_ns3/pdf 0 100000000 || exit 1
+cd .. || exit 1
+
 # Link net-device utilization plots
 cd plot_link_net_device_utilization || exit 1
 python plot_link_net_device_utilization.py ../../../build/example_run_folders/one_tcp/logs_ns3 ../../../build/example_run_folders/one_tcp/logs_ns3/data ../../../build/example_run_folders/one_tcp/logs_ns3/pdf 0 1 || exit 1
@@ -23,9 +28,4 @@ cd .. || exit 1
 cd plot_link_net_device_queue || exit 1
 python plot_link_net_device_queue.py ../../../build/example_run_folders/one_tcp/logs_ns3 ../../../build/example_run_folders/one_tcp/logs_ns3/data ../../../build/example_run_folders/one_tcp/logs_ns3/pdf 0 1 || exit 1
 python plot_link_net_device_queue.py ../../../build/example_run_folders/one_tcp/logs_ns3 ../../../build/example_run_folders/one_tcp/logs_ns3/data ../../../build/example_run_folders/one_tcp/logs_ns3/pdf 1 0 || exit 1
-cd .. || exit 1
-
-# TCP flow plots
-cd plot_tcp_flow || exit 1
-python plot_tcp_flow.py ../../../build/example_run_folders/one_tcp/logs_ns3 ../../../build/example_run_folders/one_tcp/logs_ns3/data ../../../build/example_run_folders/one_tcp/logs_ns3/pdf 0 100000000 || exit 1
 cd .. || exit 1
--- build/one_udp.sh
+++ build/one_udp.sh
@@ -13,6 +13,12 @@ bash run_assist.sh "example_run_folders/one_udp" 0 || exit 1
 # Go plot helpers
 cd ../tools/plotting || exit 1
 
+# UDP burst plots
+cd plot_udp_burst || exit 1
+python plot_udp_burst.py ../../../build/example_run_folders/one_udp/logs_ns3 ../../../build/example_run_folders/one_udp/logs_ns3/data ../../../build/example_run_folders/one_udp/logs_ns3/pdf 0 100000000 || exit 1
+python plot_udp_burst.py ../../../build/example_run_folders/one_udp/logs_ns3 ../../../build/example_run_folders/one_udp/logs_ns3/data ../../../build/example_run_folders/one_udp/logs_ns3/pdf 1 100000000 || exit 1
+cd .. || exit 1
+
 # Link net-device utilization plots
 cd plot_link_net_device_utilization || exit 1
 python plot_link_net_device_utilization.py ../../../build/example_run_folders/one_udp/logs_ns3 ../../../build/example_run_folders/one_udp/logs_ns3/data ../../../build/example_run_folders/one_udp/logs_ns3/pdf 0 1 || exit 1
@@ -23,10 +29,4 @@ cd .. || exit 1
 cd plot_link_net_device_queue || exit 1
 python plot_link_net_device_queue.py ../../../build/example_run_folders/one_udp/logs_ns3 ../../../build/example_run_folders/one_udp/logs_ns3/data ../../../build/example_run_folders/one_udp/logs_ns3/pdf 0 1 || exit 1
 python plot_link_net_device_queue.py ../../../build/example_run_folders/one_udp/logs_ns3 ../../../build/example_run_folders/one_udp/logs_ns3/data ../../../build/example_run_folders/one_udp/logs_ns3/pdf 1 0 || exit 1
-cd .. || exit 1
-
-# UDP burst plots
-cd plot_udp_burst || exit 1
-python plot_udp_burst.py ../../../build/example_run_folders/one_udp/logs_ns3 ../../../build/example_run_folders/one_udp/logs_ns3/data ../../../build/example_run_folders/one_udp/logs_ns3/pdf 0 100000000 || exit 1
-python plot_udp_burst.py ../../../build/example_run_folders/one_udp/logs_ns3 ../../../build/example_run_folders/one_udp/logs_ns3/data ../../../build/example_run_folders/one_udp/logs_ns3/pdf 1 100000000 || exit 1
 cd .. || exit 1
