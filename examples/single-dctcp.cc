fcb428a8c95b15d25c26a479306fe30b6f538f87
snkas <isniska@gmail.com>
2021-01-26 15:16:04 +0100

    Fix DCTCP programmable example

--- examples/single-dctcp.cc
+++ examples/single-dctcp.cc
@@ -36,7 +36,7 @@ namespace ns3 {
     }
 
     Ptr<Socket> TcpSocketGeneratorDctcp::GenerateTcpSocket(TypeId appTypeId, Ptr<Application> app) {
-        app->GetNode()->GetObject<TcpL4Protocol>()->SetAttribute("SocketType",  TypeIdValue(TcpVegas::GetTypeId ()));
+        app->GetNode()->GetObject<TcpL4Protocol>()->SetAttribute("SocketType",  TypeIdValue(TcpDctcp::GetTypeId ()));
         Ptr<Socket> socket = Socket::CreateSocket(app->GetNode(), TcpSocketFactory::GetTypeId());
         // Not necessary, as TcpDctcp enables it upon initialization:
         // socket->GetObject<TcpSocketBase>()->SetUseEcn(TcpSocketState::UseEcn_t::On);
